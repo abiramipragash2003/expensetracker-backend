1b58b7a9be3d8289ad83c87d520c59d6711ed107
Abirami Pragash <abirami.pragash@zucisystems.com>
2024-03-05 21:27:19 +0530

    Initial commit

---
+++ src/test/java/com/zuci/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuci.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
