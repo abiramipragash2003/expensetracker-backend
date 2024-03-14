00378925c7a983f244716b069a9ee31ac52ac202
abirami.pragash <abiramipragash2003@gmail.com>
2024-03-14 13:01:43 +0530

    refactored the code

--- src/main/java/com/zuci/expensetracker/Controller/ExpenseTrackerController.java
+++ src/main/java/com/zuci/expensetracker/Controller/ExpenseTrackerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(originPatterns = "http://127.0.0.1:5500/")
+@CrossOrigin("*")
 @RequestMapping(value = "/expensetracker")
 @RestController
 
